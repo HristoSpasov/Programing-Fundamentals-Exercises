0db5bd76df360ce6b95964991a01910e8dd5bce7
HristoSpasov <hts_arenabg@yahoo.com>
2017-02-06 12:15:41 +0200

    Add "Longest Increasing Sequence" problem to exercises from 'Lists'.

--- 10. 31.01.2017 - Lists-Exercise/Exercise/2. LIS/2. Longest Increasing Subsequence (LIS).cs
+++ 10. 31.01.2017 - Lists-Exercise/Exercise/2. LIS/2. Longest Increasing Subsequence (LIS).cs
@@ -11,36 +11,50 @@
     {
         public static void Main()
         {
-            List<int> numList = Console.ReadLine()
-                             .Split(' ')
-                             .Select(int.Parse)
-                             .ToList();
+            var sequence = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            var longestSeq = FindLongestIncreasingSubsequence(sequence);
+            //Console.WriteLine("Longest increasing subsequence (LIS)");
+            //Console.WriteLine("  Length: {0}", longestSeq.Length);
+            Console.WriteLine("{0}", string.Join(" ", longestSeq));
+        }
 
-            //numList.Add(int.MinValue);
+        public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
 
-            int currentLIS = 1;
-            int currentNumberID = 0;
-            int bestLIS = 1;
-            int bestStartID = 0;
-
-            for (int i = 0; i < numList.Count - 1; i++)
+            for (int i = 0; i < sequence.Length; i++)
             {
-                currentNumberID = i;
-                currentLIS = 1;
-                for (int j = 0; j < numList.Count; j++)
+                length[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++)
                 {
-                    int currentCompareNumber = numList[j];
-                    if (numList[j] > numList[i] && j > i)
+                    if (sequence[j] < sequence[i] && length[j] >= length[i])
                     {
-                        currentLIS++;
-                        if (currentLIS > bestLIS)
-                        {
-                            bestLIS = currentLIS;
-                            bestStartID = currentNumberID;
-                        }
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
                     }
-                }    
+                }
+
+                if (length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
+                }
             }
+
+            var longestSeq = new List<int>();
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSeq.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+            return longestSeq.ToArray();
         }
     }
 }
